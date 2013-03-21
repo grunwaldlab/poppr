387259eebe50b5257812e7d5048296420cfac72f
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2013-03-21 16:38:06 -0700

    Added rug plot to index of association distributions.

--- R/visualizations.r
+++ R/visualizations.r
@@ -164,7 +164,7 @@ poppr.plot <- function(sample, pval = c("0.05", "0.05"), pop="pop",
                        data=subset(infodata, Index==Indexfac[2]), 
                        position="identity",
                        binwidth=diff(range(subset(infodata,Index==Indexfac[2],select=Value)))/30) + 
-        
+        geom_rug() + 
         # The label for the observed line is a bit more difficult to code as
         # it has the ability to appear anywhere on the chart. Here, I'm
         # forcing it to flip to one side or the other based on which side of
@@ -198,6 +198,7 @@ poppr.plot <- function(sample, pval = c("0.05", "0.05"), pop="pop",
                        data=subset(infodata, Index==Indexfac[2]), 
                        position="identity",
                        binwidth=diff(range(subset(infodata,Index==Indexfac[2],select=Value)))/30) + 
+        geom_rug() + 
         # Positioning the observed line and labeling it.
         geom_vline(aes(xintercept=Observed), data=obsdata, color="blue", 
                    show_guide=TRUE, linetype="dashed") +
