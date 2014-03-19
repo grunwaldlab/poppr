67a67c90aa90608674f0f24f3ea85705a73640e4
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2014-03-19 13:34:20 -0700

    Moved message functions into own file.

---
+++ R/messages.r
@@ -0,0 +1,123 @@
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#
+# This software was authored by Zhian N. Kamvar and Javier F. Tabima, graduate 
+# students at Oregon State University; and Dr. Nik Grünwald, an employee of 
+# USDA-ARS.
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for educational, research and non-profit purposes, without fee, 
+# and without a written agreement is hereby granted, provided that the statement
+# above is incorporated into the material, giving appropriate attribution to the
+# authors.
+#
+# Permission to incorporate this software into commercial products may be
+# obtained by contacting USDA ARS and OREGON STATE UNIVERSITY Office for 
+# Commercialization and Corporate Development.
+#
+# The software program and documentation are supplied "as is", without any
+# accompanying services from the USDA or the University. USDA ARS or the 
+# University do not warrant that the operation of the program will be 
+# uninterrupted or error-free. The end-user understands that the program was 
+# developed for research purposes and is advised not to rely exclusively on the 
+# program for any reason.
+#
+# IN NO EVENT SHALL USDA ARS OR OREGON STATE UNIVERSITY BE LIABLE TO ANY PARTY 
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+# LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE OREGON STATE UNIVERSITY HAS BEEN ADVISED OF THE POSSIBILITY OF 
+# SUCH DAMAGE. USDA ARS OR OREGON STATE UNIVERSITY SPECIFICALLY DISCLAIMS ANY 
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE AND ANY STATUTORY 
+# WARRANTY OF NON-INFRINGEMENT. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+# BASIS, AND USDA ARS AND OREGON STATE UNIVERSITY HAVE NO OBLIGATIONS TO PROVIDE
+# MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+#==============================================================================#
+
+#==============================================================================#
+# Message to print after running the poppr function
+#
+# Public functions utilizing this function:
+# # poppr poppr.all
+#
+# Internal functions utilizing this function:
+# # none
+#==============================================================================#
+
+poppr_message <- function(){
+  cat("-----------------------------------------------------------------------|\n")
+  cat("Pop   = Population name (Total == Pooled)\n")
+  cat("N     = Census population size\n")
+  cat("MLG   = Number of unique multilocus genotypes (MLG) observed\n")
+  cat("eMLG  = Number of expected MLG based on rarefaction at smallest N >= 10\n")
+  cat("SE    = Standard error of rarefaction analysis\n")
+  cat("H     = Shannon-Wiener Index of MLG diversity\n")
+  cat("G     = Stoddart and Taylor's Index of MLG diversity\n")
+  cat("Hexp  = Nei's 1978 genotypic diversity (Expected Heterozygosity)\n")
+  cat("E.5   = Evenness\n")
+  cat("Ia    = Index of association\n")
+  cat("rbarD = Standardized index of association\n")
+  cat("-----------------------------------------------------------------------|\n")
+}
+
+#==============================================================================#
+# A function that will quit the function if a level in the hierarchy is not
+# present in the given data frame.
+#
+# Public functions utilizing this function:
+# # setpop gethierarchy poppr.amova
+#
+# Internal functions utilizing this function:
+# # make_hierarchy make_ade_df
+#==============================================================================#
+hier_incompatible_warning <- function(levs, df){
+  msg <- paste("One or more levels in the given hierarchy is not present", 
+               "in the data frame.",
+               "\nHierarchy:\t", paste(levs, collapse = ", "), "\nData:\t\t", 
+               paste(names(df), collapse = ", "))
+  return(msg)
+}
+
+#==============================================================================#
+# Warning message for when a distance matrix is non-euclidean and the user 
+# did not specify an appropriate correction.
+#
+# Public functions utilizing this function:
+# # poppr.amova
+#
+# Internal functions utilizing this function:
+# # none
+#==============================================================================#
+not_euclid_msg <- function(correction){
+  msg <- paste0("\nThe distance matrix generated is non-euclidean and a correction is needed.",
+                "\nYou supplied: correction = '", correction, "'\nPlease change",
+                " it to one of the following:\n",
+                "\t'cailliez'\t'quasieuclid'\t'lingoes'")
+  return(msg)
+}
+
+#==============================================================================#
+# Warning message for the function popsub.
+# Public functions utilizing this function:
+#
+# # popsub
+#
+# Internal functions utilizing this function:
+# # none
+#==============================================================================#
+unmatched_pops_warning <- function(pops, sublist){
+  msg <- paste("The sublist provided does not match any of the populations:\n",
+               "\tsublist.......", sublist, "\n", 
+               "\tPopulations...", paste(pops, collapse = " "))
+  return(msg)
+}
+
