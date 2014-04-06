cac2452e6efd5d8a16d46d49dac6bbfa29f23041
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2014-04-06 15:41:24 -0700

    split long examples to two lines.

--- R/visualizations.r
+++ R/visualizations.r
@@ -812,8 +812,10 @@ greycurve <- function(data = seq(0, 1, length = 1000), glim = c(0,0.8),
 #' data(microbov)
 #' micmsn <- poppr.msn(microbov, diss.dist(microbov), showplot = FALSE)
 #' 
-#' plot_poppr_msn(microbov, micmsn, palette = "terrain.colors", inds = "n", quantiles = FALSE)
-#' plot_poppr_msn(microbov, micmsn, palette = "terrain.colors", inds = "n", cutoff = 0.3, quantiles = FALSE)
+#' plot_poppr_msn(microbov, micmsn, palette = "terrain.colors", inds = "n", 
+#'   quantiles = FALSE)
+#' plot_poppr_msn(microbov, micmsn, palette = "terrain.colors", inds = "n", 
+#'   cutoff = 0.3, quantiles = FALSE)
 #' }
 #==============================================================================#
 #' @importFrom igraph layout.auto delete.edges
