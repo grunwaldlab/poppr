842d2bf53f2baefcedfcde356c07786ad26a824b
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2014-12-17 11:26:55 -0800

    Add tests to address #6

---
+++ tests/testthat/test-colors.R
@@ -0,0 +1,14 @@
+context("Graph color conversion tests")
+
+test_that("graph colors are correctly converted", {
+  library(igraph)
+  data(partial_clone)
+  pc <- partial_clone[1:11]
+  g <- bruvo.msn(pc, replen = rep(1, 10), showplot = FALSE)
+  gu <- update_poppr_graph(g, "rainbow")
+  gpc <- V(g$graph)$pie.color
+  gupc <- V(gu$graph)$pie.color
+  expect_equal(names(unlist(gpc)), names(unlist(gupc)))
+  expect_true(all(unique(unlist(gupc)) %in% rainbow(4)))
+  expect_equivalent(gupc[[4]], rainbow(4)[3:4])
+})
