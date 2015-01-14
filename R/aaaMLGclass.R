fe3a7d6b6434a9b9b54a1e02a61e835a506053f8
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-01-14 15:12:53 -0800

    Redefined the class.
    I changed the mlg slot to a data frame and removed the function because it was bloating the object. I also added it to be internal.

--- R/aaaMLGclass.R
+++ R/aaaMLGclass.R
@@ -8,16 +8,14 @@
 #' @export
 #' @slot mlg a list containing four vectors, one for each type of MLG manipulation.
 #' @slot visible a character specifying which MLG type is to be displayed and accessed.
-#' @slot dist a function to calculate the genetic distance for collapsing MLGs.
-#' @slot distname the name of the distance function
-#' @slot Two numbers specifying the cutoff value for expanding and collapsing
+#' @slot distname the name of the distance function used to collapse mlgs.
+#' @slot cutoff Two numbers specifying the cutoff value for expanding and collapsing
 #' MLGs.
 #' @author Zhian N. Kamvar
-#' @seealso \code{\link{as.genclone}} \code{\link{sethierarchy}} \code{\link{setpop}} 
-#' \code{\linkS4class{genind}} 
+#' @seealso \code{\linkS4class{genclone}}
+#' @keywords internal
 setClass("MLG", 
          representation(visible = "character",
                         cutoff = "numeric",
-                        dist = "function",
                         distname = "character",
-                        mlg = "list"))
+                        mlg = "data.frame"))
