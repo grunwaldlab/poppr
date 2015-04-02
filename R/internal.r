21d72223cd810ed07eba14a80781cdc8fe5b0c2d
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-04-02 15:28:04 -0700

    spacing in bruvos_distance

--- R/internal.r
+++ R/internal.r
@@ -1210,22 +1210,29 @@ bruvos_distance <- function(bruvomat, funk_call = match.call(), add = TRUE,
   ploid  <- bruvomat@ploidy
   replen <- bruvomat@replen
   x[is.na(x)] <- 0
+
   # Dividing the data by the repeat length of each locus.
-  x <- x / rep(replen, each=ploid*nrow(x))
-  x <- matrix(as.integer(round(x)), ncol=ncol(x))
+  x <- x / rep(replen, each = ploid * nrow(x))
+  x <- matrix(as.integer(round(x)), ncol = ncol(x))
+
   # Getting the permutation vector.
   perms <- .Call("permuto", ploid, PACKAGE = "poppr")
+
   # Calculating bruvo's distance over each locus. 
   distmat <- .Call("bruvo_distance", x, perms, ploid, add, loss, PACKAGE = "poppr")
+
   # If there are missing values, the distance returns 100, which means that the
   # comparison is not made. These are changed to NA.
   distmat[distmat == 100] <- NA
+
   # Obtaining the average distance over all loci.
   avg.dist.vec <- apply(distmat, 1, mean, na.rm=TRUE)
+
   # presenting the information in a lower triangle distance matrix.
   dist.mat <- matrix(ncol=nrow(x), nrow=nrow(x))
   dist.mat[which(lower.tri(dist.mat)==TRUE)] <- avg.dist.vec
   dist.mat <- as.dist(dist.mat)
+
   attr(dist.mat, "Labels") <- bruvomat@ind.names
   attr(dist.mat, "method") <- "Bruvo"
   attr(dist.mat, "call")   <- funk_call
