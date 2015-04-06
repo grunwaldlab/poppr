6344e39eed83ccae418d395412d38329282732cf
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-04-06 13:42:15 -0700

    add new test for polyploids

--- tests/testthat/test-polyploids.R
+++ tests/testthat/test-polyploids.R
@@ -13,3 +13,16 @@ test_that("recode_polyploids won't take diploid data", {
 	data(nancycats, package = "adegenet")
 	expect_warning(recode_polyploids(nancycats), "Input is not a polyploid data set, returning original.")
 })
+
+test_that("recode_polyploids will go there and back again", {
+	skip_on_cran()
+	data(Pinf, package = "poppr")
+	expect_warning(pr <- recode_polyploids(Pinf, addzero = TRUE), "addzero = TRUE")
+	pr <- recode_polyploids(Pinf, newploidy = TRUE)
+	expect_that(length(unique(ploidy(pr))), equals(2))
+	zpr <- recode_polyploids(pr, addzero = TRUE)
+	expect_that(length(unique(ploidy(zpr))), equals(1))
+	przpr <- recode_polyploids(zpr, newploidy = TRUE)
+	rectab <- tab(przpr)
+	expect_equivalent(rectab[, colnames(tab(pr))], tab(pr))
+})
