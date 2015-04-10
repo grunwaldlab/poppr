1042c0807071f3ec0b7184dd551a8d853a49193a
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-04-10 09:56:32 -0700

    add better test for fix_negative_branch.

--- tests/testthat/test-values.R
+++ tests/testthat/test-values.R
@@ -42,17 +42,22 @@ test_that("Index of association works as expected.", {
 })
 
 test_that("Internal function fix_negative_branch works as expected.", {
-  the_distance <- structure(c(0, 0.05, 0, 0, 0.05, 0, 0.05, 0.075, 0.05, 0.05, 
-            0.075, 0.05, 0, 0, 0.05, 0, 0.05, 0.075, 0.05, 0.05, 0.075, 0.05, 
-            0.05, 0, 0.05, 0, 0.05, 0.1, 0, 0.05, 0, 0.05, 0, 0.05, 0.075, 
-            0.05, 0.05, 0.075, 0.05, 0, 0.05, 0.075, 0.05, 0.05, 0.075, 0.05, 
-            0, 0.05, 0.1, 0, 0.05, 0.05, 0.075, 0.05, 0.05, 0.075, 0.05, 
-            0.1, 0, 0.05, 0.125, 0.05, 0, 0.1, 0.125, 0.05), Size = 12L, class = "dist", 
-            Diag = FALSE, Upper = FALSE, Labels = 1:12, method = "Bruvo")
-  the_tree <- ape::nj(the_distance)
+  the_tree <- structure(list(edge = structure(c(9L, 10L, 11L, 12L, 13L, 14L,
+      14L, 13L, 12L, 11L, 10L, 9L, 9L, 10L, 11L, 12L, 13L, 14L, 2L, 3L, 6L, 7L,
+      8L, 1L, 5L, 4L), .Dim = c(13L, 2L)), 
+      edge.length = c(0, 0.0625, 0.0625, 0.09375, 0.15, -0.25, 0.25, -0.15,
+      -0.09375, -0.0625, 0, 0, 0),
+      tip.label = c("2340_50156.ab1 ", "2340_50149.ab1 ", "2340_50674.ab1 ",
+      "2370_45312.ab1 ", "2340_50406.ab1 ", "2370_45424.ab1 ", "2370_45311.ab1
+      ", "2370_45521.ab1 "), 
+      Nnode = 6L
+    ),
+    .Names = c("edge", "edge.length", "tip.label", "Nnode"), 
+    class = "phylo",
+    order = "cladewise")
   fix_tree <- poppr:::fix_negative_branch(the_tree)
   # Not all branch lengths are positive
-  # expect_false(min(the_tree$edge.length) >= 0)
+  expect_false(min(the_tree$edge.length) >= 0)
   # After fix, all branch lengths are positive
   expect_true(min(fix_tree$edge.length) >= 0)
   # The difference from fixed and unfixed is unfixed. This indicates that the
