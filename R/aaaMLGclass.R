d1a6db15e983310decd507a48658291c1dbb7fe5
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-04-23 11:04:31 -0700

    Add documentation

--- R/aaaMLGclass.R
+++ R/aaaMLGclass.R
@@ -1,9 +1,11 @@
+setClassUnion("charORLang", c("character", "language"))
 #' MLG class
 #' 
 #' A class to store multilocus genotypes in genclone objects. 
 #' 
 #' @name MLG-class
 #' @rdname MLG-class
+#' @aliases MLG
 #' @export
 #' @slot mlg a list containing four vectors, one for each type of MLG manipulation.
 #' @slot visible a character specifying which MLG type is to be displayed and accessed.
@@ -14,7 +16,6 @@
 #' @author Zhian N. Kamvar
 #' @seealso \code{\linkS4class{genclone}} \code{\link{mll}} \code{\linkS4class{snpclone}}
 #' @keywords internal
-setClassUnion("charORLang", c("character", "language"))
 setClass("MLG", 
          representation(visible = "character",
                         cutoff = "numeric",
