6395aee63e9d14ff66f32d4f63e6367358930629
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-04-23 10:50:10 -0700

    moved mlg.filter to a method

--- R/aaaMLGclass.R
+++ R/aaaMLGclass.R
@@ -8,13 +8,16 @@
 #' @slot mlg a list containing four vectors, one for each type of MLG manipulation.
 #' @slot visible a character specifying which MLG type is to be displayed and accessed.
 #' @slot distname the name of the distance function used to collapse mlgs.
+#' @slot distargs the arguments provided to compute the distance function.
 #' @slot cutoff Two numbers specifying the cutoff value for expanding and collapsing
 #' MLGs.
 #' @author Zhian N. Kamvar
 #' @seealso \code{\linkS4class{genclone}} \code{\link{mll}} \code{\linkS4class{snpclone}}
 #' @keywords internal
+setClassUnion("charORLang", c("character", "language"))
 setClass("MLG", 
          representation(visible = "character",
                         cutoff = "numeric",
-                        distname = "character",
+                        distname = "charORLang",
+                        distargs = "list",
                         mlg = "data.frame"))
