694a397f69264b4ae7e132101349416fbccc4b77
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-03 09:47:31 -0700

    add bitwise tests for ploidy issues

--- tests/testthat/test-bitwise.R
+++ tests/testthat/test-bitwise.R
@@ -1,4 +1,33 @@
 context("Bitwise distance and pgen calculations")
+set.seed(991)
+dat <- sample(c(0, 1, NA), 50, replace = TRUE, prob = c(0.49, 0.5, 0.01))
+mat <- matrix(dat, nrow = 5, ncol = 10)
+
+test_that("bitwise.dist works for haploids", {
+  mat.gl <- new("genlight", mat, parallel = FALSE)
+  expected <- rowSums(apply(mat, 2, dist), na.rm = TRUE)/10
+  expect_equivalent(as.vector(bitwise.dist(mat.gl)), expected)
+  expect_true(all(ploidy(mat.gl) == 1))
+})
+
+test_that("bitwise.dist works for diploids with only one type of homozygote", {
+  mat2 <- mat
+  mat2[-1, ] <- mat2[-1, ] * 2
+  mat2.gl <- new("genlight", mat2, parallel = FALSE)
+  expect_error(bitwise.dist(mat2.gl), "ploidy")
+  ploidy(mat2.gl) <- rep(2, 5)
+  expected <- rowSums(apply(mat2, 2, dist), na.rm = TRUE)/20
+  expect_equivalent(as.vector(bitwise.dist(mat2.gl)), expected)
+})
+
+
+test_that("bitwise.dist can actually handle genind objects", {
+  skip_on_cran()
+  data("partial_clone", package = "poppr")
+  pdist <- diss.dist(partial_clone, percent = TRUE)
+  expect_equivalent(pdist, bitwise.dist(partial_clone))
+})
+
 
 test_that("bitwise.dist produces reasonable results", {
 
@@ -45,22 +74,8 @@ skip_on_cran()
   #expect_equivalent(missing_nomatch, expected_nomatch)
  })
 
-test_that("bitwise.dist works for haploids", {
-  set.seed(991)
-  dat <- sample(c(0, 1, NA), 50, replace = TRUE, prob = c(0.49, 0.5, 0.01))
-  mat <- matrix(dat, nrow = 5, ncol = 10)
-  mat.gl <- new("genlight", mat, parallel = FALSE)
-  expected <- rowSums(apply(mat, 2, dist), na.rm = TRUE)/10
-  expect_equivalent(as.vector(bitwise.dist(mat.gl)), expected)
-  expect_true(all(ploidy(mat.gl) == 1))
-})
 
-test_that("bitwise.dist can actually handle genind objects", {
-  skip_on_cran()
-  data("partial_clone", package = "poppr")
-  pdist <- diss.dist(partial_clone, percent = TRUE)
-  expect_equivalent(pdist, bitwise.dist(partial_clone))
-})
+
 
 test_that("pgen produces reasonable results", {
 
