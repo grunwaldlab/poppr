fe5c36c5488c28de8ef965c231be43bbf72cc28d
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-03 11:57:03 -0700

    add more tests for amova

--- tests/testthat/test-amova.R
+++ tests/testthat/test-amova.R
@@ -1,10 +1,15 @@
 context("Amova tests")
 
+data("Aeut", package = "poppr")
+strata(Aeut) <- other(Aeut)$population_hierarchy[-1]
+
+agc    <- as.genclone(Aeut)
+Athena <- popsub(agc, "Athena")
+res    <- poppr.amova(Aeut, ~Pop/Subpop, quiet = TRUE)
+rescc  <- poppr.amova(Aeut, ~Pop/Subpop, quiet = TRUE, clonecorrect = TRUE)
+
+
 test_that("Amova returns published values", {
-	data(Aeut, package = "poppr")
-  strata(Aeut) <- other(Aeut)$population_hierarchy[-1]
-	res   <- poppr.amova(Aeut, ~Pop/Subpop, quiet = TRUE)
-	rescc <- poppr.amova(Aeut, ~Pop/Subpop, quiet = TRUE, clonecorrect = TRUE)
 	expect_that(res$componentsofcovariance[, 2], equals(c(70.0067859292295, 
 														  8.40748251295027, 
 														  21.5857315578203, 
@@ -23,10 +28,6 @@ test_that("Amova returns published values", {
 															15.593761991347)))
 	})
 test_that("AMOVA handles subsetted genclone objects", {
-	data(Aeut, package = "poppr")
-  strata(Aeut) <- other(Aeut)$population_hierarchy[-1]
-	agc <- as.genclone(Aeut)
-	Athena <- popsub(agc, "Athena")
 	Athena.mlg <- mlg.vector(Athena)
 	agc.mlg <- mlg.vector(agc)
 	Athena.AMOVA <- poppr.amova(Athena, ~Subpop, quiet = TRUE)
@@ -40,3 +41,48 @@ test_that("AMOVA handles subsetted genclone objects", {
 	#AMOVA will still work on the data set with missing MLGs
 	expect_that(class(Athena.AMOVA), equals("amova"))	
 	})
+
+
+test_that("AMOVA can take a distance matrix as input", {
+  skip_on_cran()
+  adist   <- diss.dist(clonecorrect(Aeut, strata = NA), percent = FALSE)
+  root_adist  <- sqrt(adist)
+  
+  # UNSQUARED
+  usqres <- poppr.amova(Aeut, ~Pop/Subpop, quiet = TRUE, dist = root_adist, 
+                         squared = FALSE)
+  # SQUARED
+  sqres <- poppr.amova(Aeut, ~Pop/Subpop, quiet = TRUE, dist = adist, 
+                        squared = TRUE)
+  
+  expect_equivalent(usqres$componentsofcovariance, res$componentsofcovariance)
+  expect_equivalent(usqres$componentsofcovariance, sqres$componentsofcovariance)
+  err <- c("Uncorrected.+?97.+?Clone.+?70.+?provided.+?119")
+  expect_error(poppr.amova(Athena, ~Subpop, quiet = TRUE, dist = adist), err)
+})
+
+
+test_that("AMOVA can work on clone correction from mlg.filter", {
+  skip_on_cran()
+  data("monpop", package = "poppr")
+  splitStrata(monpop) <- ~Tree/Year/Symptom
+  expect_warning(poppr.amova(monpop, ~Symptom/Year), "Zero")
+  mondist <- diss.dist(monpop)
+  monfilt <- mlg.filter(monpop, distance = mondist, threshold = 1.1, 
+                        stats = "DIST")
+  mlg.filter(monpop, distance = mondist) <- 1.1 
+  monfiltdist <- as.dist(monfilt/nLoc(monpop))
+  monres <- poppr.amova(monpop, ~Symptom/Year, dist = monfiltdist)
+  expect_equivalent(monres$componentsofcovariance[4, 2], 100)
+})
+
+test_that("AMOVA can calculate within individual variance for diploids", {
+  skip_on_cran()
+  data("microbov", package = "adegenet")
+  strata(microbov) <- data.frame(other(microbov))
+  mics <- microbov[pop = 1:2]
+  expect_output(micwithin  <- poppr.amova(mics, ~breed), "Removing")
+  expect_output(micwithout <- poppr.amova(mics, ~breed, within = FALSE), "Removing")
+  expect_equal(dim(micwithin$componentsofcovariance), c(4, 2))
+  expect_equal(dim(micwithout$componentsofcovariance), c(3, 2))
+})
