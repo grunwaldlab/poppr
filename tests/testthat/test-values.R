f132647f97faf4ba4577c0785eae8df3cbfedb2d
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-03 11:56:41 -0700

    add win.ia and samp.ia tests

--- tests/testthat/test-values.R
+++ tests/testthat/test-values.R
@@ -7,10 +7,11 @@ test_that("Bruvo's distance works as expected.", {
   ADDloss <- as.vector(bruvo.dist(testgid, add = TRUE, loss = FALSE))
   addLOSS <- as.vector(bruvo.dist(testgid, add = FALSE, loss = TRUE))
   ADDLOSS <- as.vector(bruvo.dist(testgid, add = TRUE, loss = TRUE))
-  expect_that(addloss, equals(0.46875000000000))
-  expect_that(addLOSS, equals(0.34374987334013))
-  expect_that(ADDloss, equals(0.458333164453506))
-  expect_that(ADDLOSS, equals(0.401041518896818))
+  # Values from Bruvo et. al. (2004)
+  expect_equal(addloss, 0.46875000000000)
+  expect_equal(addLOSS, 0.34374987334013)
+  expect_equal(ADDloss, 0.458333164453506)
+  expect_equal(ADDLOSS, 0.401041518896818)
 })
 
 test_that("Infinite Alleles Model works.",{
@@ -20,7 +21,7 @@ test_that("Infinite Alleles Model works.",{
                  class = "data.frame")
   gid <- df2genind(x, sep = "/", ploidy = 3)
   res <- bruvo.dist(gid, replen = c(2/10000,2/10000), add = FALSE, loss = FALSE)
-  expect_that(as.numeric(res), equals(0.833333333333333))
+  expect_equal(as.numeric(res), 0.833333333333333)
 })
 
 test_that("Dissimilarity distance works as expected.", {
@@ -29,16 +30,15 @@ test_that("Dissimilarity distance works as expected.", {
   nanmat <- diss.dist(nan1, mat = TRUE)
   expect_that(diss.dist(nan1), is_a("dist"))
   expect_that(nanmat, is_a("matrix"))
-  #expect_that(diss.dist(nan1, mat = TRUE, percent = FALSE), equals(nanmat*2*9))
-  #expect_that(nanmat[2, 1], equals(0.222222222222222))
-  expect_that(diss.dist(nan1, mat = TRUE, percent = TRUE), equals((nanmat/2)/9))
-  expect_that(nanmat[2, 1], equals(4))
+  expect_equal(diss.dist(nan1, mat = TRUE, percent = TRUE), (nanmat/2)/9)
+  expect_equal(nanmat[2, 1], 4)
 })
 
 test_that("Index of association works as expected.", {
   data(Aeut, package = "poppr")
+  # Values from Grünwald and Hoheisel (2006)
   res <- c(Ia = 14.3707995986407, rbarD = 0.270617053778004)
-  expect_that(ia(Aeut), equals(res))
+  expect_equal(ia(Aeut), res)
 })
 
 test_that("Internal function fix_negative_branch works as expected.", {
@@ -139,6 +139,35 @@ test_that("ia and pair.ia return same values", {
   expect_equivalent(pc_pair[pair_posi], pc_ia)
 })
 
+
+test_that("win.ia produces expected results", {
+  skip_on_cran()
+  set.seed(999)
+  x <- glSim(n.ind = 10, n.snp.nonstruc = 5e2, n.snp.struc = 5e2, ploidy = 2)
+  position(x) <- sort(sample(1e4, 1e3))
+  pos.res <- c(0.0455840455840456, -0.121653107452204, -0.00595292101094314,
+               0.0080791417110234, -0.0168361601753453, -0.00529454073927957,
+               -0.00897633972053186, -0.0127370947405975, -0.0449102196309011,
+               -0.00973722044247706, -0.00395345516884293, -0.00365271629340326,
+               -0.00974233012233703, -0.00245476433207628, 0.00284396024347222,
+               -0.00961958529835491, 0.0131920863329584, 0.153481745744672,
+               0.267842562306642, 0.241714085779086, 0.218062029786597,
+               0.160460027459485, 0.351036788453414, 0.165485011482322,
+               0.275776270167356, 0.292113177590906, 0.201538950412372,
+               0.146277858717017, 0.171243142808176, 0.213214941672605,
+               0.200245399275377, 0.243007310007378, 0.12396191060666,
+               0.0350644169627312 )
+
+  nopos.res <- c(0.00124186533451839, 0.0334854151956646, 0.226632983481153, 
+                 0.173916930910343)
+
+  win.pos     <- win.ia(x, window = 300L, quiet = TRUE)
+  position(x) <- NULL
+  win.nopos   <- win.ia(x, window = 300L, quiet = TRUE)
+  expect_equivalent(win.pos, pos.res)
+  expect_equivalent(win.nopos, nopos.res)
+})
+
 test_that("win.ia knows the batman theme", {
   skip_on_cran()
   set.seed(999)
@@ -146,6 +175,22 @@ test_that("win.ia knows the batman theme", {
   position(x) <- sort(sample(1e4, 1e3))
   res <- win.ia(x, window = 300L, min.snps = 100L, quiet = TRUE)
   expect_true(all(is.na(res)))
+})
+
+test_that("samp.ia works",{
+  skip_on_cran()
+  set.seed(999)
+  x <- glSim(n.ind = 10, n.snp.nonstruc = 5e2, n.snp.struc = 5e2, ploidy = 2)
+  position(x) <- sort(sample(1e4, 1e3))
+  set.seed(900)
+  pos.res <- samp.ia(x, n.snp = 20, reps = 2, quiet = TRUE)
+  expect_equivalent(pos.res, c(0.0754028380744677, 0.0178846504949451))
+
+  # Sampling is agnostic to position
+  position(x) <- NULL
+  set.seed(900)
+  nopos.res <- samp.ia(x, n.snp = 20, reps = 2, quiet = TRUE)
+  expect_equivalent(nopos.res, pos.res)
 })
 
 test_that("fix_replen works as expected", {
