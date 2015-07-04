5b9a6269d40527a8355d3f0c6b3604db2b7b0348
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-04 12:49:18 -0700

    add tests to distpop

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -10,7 +10,7 @@ x  <- as.genclone(df2genind(grid_example, ploidy = 1))
 xd <- dist(grid_example)
 
 set.seed(999)
-gc <- as.snpclone(glSim(100, 0, n.snp.struc = 1e3, ploidy = 2))
+gc <- as.snpclone(glSim(100, 0, n.snp.struc = 1e3, ploidy = 2, parallel = FALSE))
 
 
 test_that("multilocus genotype filtering algorithms work", {
@@ -47,48 +47,52 @@ test_that("mlg.filter can remember things", {
   suppressWarnings(mlg.filter(gc) <- 0)
   
   # Code is shown after modification and values are set to 0, nei and farthest
-  expect_false(all( !grepl(tda, capture.output(x)) ))
+  expect_output(x, tda)
   original_vals <- "(0).+?(nei.dist).+?(farthest).+?"
   gc_original_vals <- "(0).+?(bitwise.dist).+?(farthest).+?"
-  expect_false(all( !grepl(original_vals, capture.output(x)) ))
-  expect_false(all( !grepl(gc_original_vals, capture.output(gc)) ))
+  expect_output(x, original_vals)
+  expect_output(gc, gc_original_vals)
   
   # supplied distance matrices work
   assign("x20150702210257_distance", xd, envir = .GlobalEnv)
   assign("x20150703173505_distance", bitwise.dist(gc, differences_only = TRUE), envir = .GlobalEnv)
   mlg.filter(x, distance = x20150702210257_distance) <- 0
   mlg.filter(gc, distance = x20150703173505_distance) <- 0
-  expect_false(all( !grepl("x20150702210257_distance", capture.output(x)) ))
-  expect_false(all( !grepl("x20150703173505_distance", capture.output(gc)) ))
+  expect_output(x, "x20150702210257_distance")
+  expect_output(gc, "x20150703173505_distance")
   
   # choosing different algorithms work
   mlg.filter(x, algo = "average") <- 0
-  expect_false(all( !grep("average", capture.output(x)) ))
   mlg.filter(gc, algo = "average") <- 0
-  expect_false(all( !grep("average", capture.output(gc)) ))
+  expect_output(x, "average")
+  expect_output(gc, "average")
   
+  # and the distance is still persistent
+  expect_output(x, "x20150702210257_distance")
+  expect_output(gc, "x20150703173505_distance")
+
   # choosing different distances with parameters works
   mlg.filter(x, distance = diss.dist, percent = TRUE) <- 0
   expect_equal(nmll(x), 5)
-  expect_false(all( !grep("diss.dist", capture.output(x)) ))
+  expect_output(x, "diss.dist")
   
   mlg.filter(gc, distance = bitwise.dist, percent = FALSE) <- 0
   expect_equal(nmll(gc), 100)
-  expect_false(all( !grep("bitwise.dist", capture.output(gc)) ))
+  expect_output(gc, "bitwise.dist")
   
   # algorithms are persistant
-  expect_false(all( !grep("average", capture.output(x)) ))
-  expect_false(all( !grep("average", capture.output(gc)) ))
+  expect_output(x, "average")
+  expect_output(gc, "average")
   
   # Parameters are kept (default for diss.dist is percent = FALSE)
   mlg.filter(x) <- 0.75
   expect_equal(nmll(x), 3)
-  expect_false(all( !grep("0.75", capture.output(x)) ))
+  expect_output(x, "0.75")
   
   # (default for bitwise.dist is percent = TRUE)
   mlg.filter(gc) <- 500
   expect_equal(nmll(gc), 81)
-  expect_false(all( !grep("500", capture.output(gc)) ))
+  expect_output(gc, "500")
   
   mlg.filter(x, distance = x20150702210257_distance) <- 4.51
   expect_equal(nmll(x), 2)
