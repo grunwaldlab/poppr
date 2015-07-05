f61e98a83a606b4f7bc93521b951530e4d88463e
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-05 13:46:41 -0700

    add explicit internal function call

--- tests/testthat/test-colors.R
+++ tests/testthat/test-colors.R
@@ -4,7 +4,7 @@ test_that("graph colors are correctly converted", {
   data(partial_clone)
   pc <- partial_clone[1:11]
   g <- bruvo.msn(pc, replen = rep(1, 10), showplot = FALSE)
-  gu <- update_poppr_graph(g, "rainbow")
+  gu <- poppr:::update_poppr_graph(g, "rainbow")
   gpc <- igraph::V(g$graph)$pie.color
   gupc <- igraph::V(gu$graph)$pie.color
   expect_equal(names(unlist(gpc)), names(unlist(gupc)))
