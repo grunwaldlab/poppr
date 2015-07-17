fe2f1fec9974ea538c0ba03479121b541b3f464f
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-16 20:36:04 -0700

    fixed tests for locustable because of #47

--- tests/testthat/test-locustable.R
+++ tests/testthat/test-locustable.R
@@ -9,10 +9,10 @@ test_that("locus_table correctly treats polyploids", {
 	pinflt <- locus_table(Pinf)
 	pinfpp <- poppr(Pinf, quiet = TRUE)
 	expect_equivalent(pinflt[-(nLoc(Pinf) + 1), "allele"], nAll(Pinf) - 1)
-	expect_equivalent(pinflt["mean", "1-D"], pinfpp[pinfpp$Pop == "Total", "lambda"])
+	expect_equivalent(pinflt["mean", "uSimpson"], pinfpp[pinfpp$Pop == "Total", "Hexp"])
 	salt   <- locus_table(Pinf, population = "South America")
 	expect_equivalent(salt["Pi33", "allele"], 1)
-	expect_equivalent(salt["mean", "1-D"], pinfpp[pinfpp$Pop == "South America", "lambda"])
+	expect_equivalent(salt["mean", "uSimpson"], pinfpp[pinfpp$Pop == "South America", "Hexp"])
 })
 
 test_that("locus_table presents different stats", {
