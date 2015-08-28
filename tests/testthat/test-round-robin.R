757276b628ab8f182e6ea2501e56718f970d7fae
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-08-28 11:56:09 -0700

    add tests for psex and pgen

--- tests/testthat/test-round-robin.R
+++ tests/testthat/test-round-robin.R
@@ -57,3 +57,53 @@ test_that("rraf calculates per population", {
   expect_equivalent(dim(rrx_matrix), c(nPop(Pram), ncol(tab(Pram))))
   expect_equivalent(rownames(rrx_matrix), popNames(Pram))
 })
+
+test_that("psex and pgen internals produce expected results", {
+  skip_on_cran()
+  # From Parks and Werth, 1993
+  x <- "
+ Hk Lap Mdh2 Pgm1 Pgm2 X6Pgd2
+54 12 12 12 23 22 11
+36 22 22 11 22 33 11
+10 23 22 11 33 13 13"
+  
+  xtab <- read.table(text = x, header = TRUE, row.names = 1)
+  xgid <- df2genind(xtab[rep(rownames(xtab), as.integer(rownames(xtab))), ], ncode = 1)
+  afreq <- c(Hk.1 = 0.167, Hk.2 = 0.795, Hk.3 = 0.038, 
+             Lap.1 = 0.190, Lap.2 = 0.798, Lap.3 = 0.012,
+             Mdh2.0 = 0.011, Mdh2.1 = 0.967, Mdh2.2 = 0.022,
+             Pgm1.2 = 0.279, Pgm1.3 = 0.529, Pgm1.4 = 0.162, Pgm1.5 = 0.029,
+             Pgm2.1 = 0.128, Pgm2.2 = 0.385, Pgm2.3 = 0.487,
+             X6Pgd2.1 = 0.526, X6Pgd2.2 = 0.051, X6Pgd2.3 = 0.423)
+  freqs <- afreq[colnames(tab(xgid))]
+  pops <- rep(1L, nInd(xgid))
+  pgenmat <- .Call("get_pgen_matrix_genind", xgid, freqs, pops, 1L, PACKAGE = "poppr")
+  dimnames(pgenmat) <- list(indNames(xgid), locNames(xgid))
+  G <- 45 # Number of MLGs observed
+  xpgen <- exp(rowSums(pgenmat))
+  pNotGen <- (1 - xpgen)^G
+  1 - pNotGen
+  res <- matrix(c( unique(xpgen), unique(1 - pNotGen)), ncol = 2)
+  
+  expected_result <- structure(c(4.14726753733799e-05, 0.00192234266749449, 0.000558567714432373, 
+                                 0.00186456862059092, 0.0829457730256321, 0.024829127718338), 
+                               .Dim = c(3L,2L))
+  expect_equivalent(res, expected_result)
+  
+})
+
+test_that("psex produces a vector", {
+  skip_on_cran()
+  data(Pram)
+  psexpram <- psex(Pram)
+  expect_is(psexpram, "numeric")
+  expect_equal(length(psexpram), nInd(Pram))
+})
+
+test_that("pgen produces a matrix", {
+  skip_on_cran()
+  data(Pram)
+  pgenlog <- pgen(Pram, by_pop = FALSE)
+  expect_is(pgenlog, "matrix")
+  expect_equivalent(exp(pgenlog), pgen(Pram, by_pop = FALSE, log = FALSE))
+})
