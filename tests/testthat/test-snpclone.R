c3647328b7c50f8792e4c2336201ac408de0e0df
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2017-04-02 20:05:21 -0500

    add TODO statements to test-snpclone
    This is to address #125

--- tests/testthat/test-snpclone.R
+++ tests/testthat/test-snpclone.R
@@ -1,15 +1,25 @@
 context("snpclone coercion tests")
 
 set.seed(999)
-gc <- as.snpclone(glSim(100, 0, n.snp.struc = 1e3, ploidy = 2, parallel = FALSE), parallel = FALSE)
-indNames(gc) <- paste("sample", seq(nInd(gc)))
-mlg.filter(gc, threads = 1L) <- 0.25
+gl <- glSim(50, 0, n.snp.struc = 1e3, ploidy = 2, parallel = FALSE)
+
+indNames(gl) <- paste("sample", seq(nInd(gl)))
+sc           <- as.snpclone(gl, parallel = FALSE)
+mlg.filter(sc, threads = 1L) <- 0.25
 
 
 test_that("subsetting a snpclone object retains the MLG definitions", {
-  idn <- mlg.id(gc)[[2]]
-  idl <- indNames(gc) %in% idn
+  idn <- mlg.id(sc)[[2]]
+  idl <- indNames(sc) %in% idn
   idi <- which(idl)
-  expect_equal(mll(gc[idn]), mll(gc[idl]))
-  expect_equal(mll(gc[idi]), mll(gc[idl]))
+  expect_equal(mll(sc[idn]), mll(sc[idl]))
+  expect_equal(mll(sc[idi]), mll(sc[idl]))
 })
+
+# TODO: test mlg.vector on both gl and sc. They should be the same
+
+# TODO: test for mlg.reset argument by assigning mlg = seq_len(nInd(gl)) in as.snpclone
+
+# TODO: test for handling of missing data (should be considered new genotype)
+
+# TODO: test to see if missing data at the same locus is considered similar or different
