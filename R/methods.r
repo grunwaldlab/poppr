3958a457b32c81cb1847038fd95ce0bb6b6eca7b
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2017-04-13 09:57:00 -0500

    update as.clone documentation

--- R/methods.r
+++ R/methods.r
@@ -285,7 +285,8 @@ setMethod(
 #' @export
 #' @rdname is.clone
 #' @examples
-#' (sc <- as.snpclone(glSim(100, 1e3, ploid=2)))
+#' (sc <- as.snpclone(glSim(100, 1e3, ploid=2, parallel = FALSE), 
+#'                    parallel = FALSE, n.cores = 1L))
 #' is.snpclone(sc)
 #==============================================================================#
 is.snpclone <- function(x){
