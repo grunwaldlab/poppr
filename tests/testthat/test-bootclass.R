531536c11fe0d853382724fcbc5117cd3be541cf
Zhian Kamvar <zkamvar@gmail.com>
2017-12-09 17:45:09 -0600

    add tests for bootclass

--- tests/testthat/test-bootclass.R
+++ tests/testthat/test-bootclass.R
@@ -8,6 +8,28 @@ nanreps <- structure(c(1.99999, 2, 1.99999, 2, 2, 2, 1.99999, 1.99999, 2
 bgnan <- new("bootgen", nan9, na = "asis", freq = FALSE)
 bvnan <- new("bruvomat", nan9, nanreps)
 
+test_that("an empty bootgen can be initialized", {
+  expect_is(new("bootgen"), "bootgen")
+})
+
+test_that("an empty bruvomat can be initialized", {
+  expect_is(new("bruvomat"), "bruvomat")
+})
+
+test_that("bruvomat will guess repeat lengths if given none", {
+  skip_on_cran()
+  expect_is(new("bruvomat", nan9), "bruvomat")
+})
+
+test_that("bootgen throws an error if a non-gen object is passed", {
+  expect_error(new("bootgen", nanreps), 
+               gettext("gen must be a valid genind or genpop object", domain = "R"))
+})
+
+test_that("bootgen can be initialized from a genppop", {
+  skip_on_cran()
+  expect_is(new("bootgen", genind2genpop(nancycats, quiet = TRUE)), "bootgen")
+})
 test_that("bootgen can be subset correctly without loci specified", {
   expect_identical(bgnan[], bgnan)
 })
@@ -16,9 +38,18 @@ test_that("bruvomat can be subset correctly without loci specified", {
   expect_equivalent(bvnan[], bvnan)
 })
 
+test_that("bootgen throws an error if given out-of-bounds subscripts", {
+  expect_error(bgnan[, 10], gettext("subscript out of bounds", domain = "R"))
+})
+
 test_that("bruvomat can be subset correctly without loci specified", {
   subafter <- bvnan@mat[, rep((1:9) %% 2 == 1, each = 2)]
   subfore  <- bvnan[, (1:9) %% 2 == 1]@mat
   dimnames(subafter) <- NULL -> dimnames(subfore)
   expect_equivalent(subfore, subafter)
+})
+
+test_that("dist works with bootgen", {
+  skip_on_cran()
+  expect_is(dist(bgnan), "dist")
 })
