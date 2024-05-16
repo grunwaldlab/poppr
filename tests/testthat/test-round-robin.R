7173e2c1ac49f1297ff88cbac671f46daf98b4e4
Zhian N. Kamvar <zkamvar@gmail.com>
2024-05-16 10:40:59 -0700

    fix test debug

--- tests/testthat/test-round-robin.R
+++ tests/testthat/test-round-robin.R
@@ -73,7 +73,7 @@ test_that("correction is properly applied in rraf", {
   # sum_to_one argument augments does what it says
   monc_sum2one <- vapply(monc_sto, sum, numeric(1))
   expect_equal(sum(monc_sum2one), nLoc(monpop))
-  expect_true(all(monc_vec == monc_sum2one), 
+  expect_true(all(monc_vec >= monc_sum2one), 
     label = paste0(paste(capture.output(
       data.frame(vec = monc_vec, sum2one = monc_sum2one)
     ), collapse = "\n"), "\n")
